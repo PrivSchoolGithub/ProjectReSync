ac27eda10f4680fac6f4ac6812abe4c996861781
MasterKingSirPlease <52339239+MasterKingSirPlease@users.noreply.github.com>
2022-04-05 19:59:45 -0400

    Create Core.lua

---
+++ Templates/Core.lua
@@ -0,0 +1,47 @@
+--[[
+                          _____    _____  _____              
+                         |  __ \  / ____||_   _|              
+                         | |__) || (___    | |
+                         |  _  /  \___ \   | |
+                         | | \ \  ____) | _| |_
+                         |_|  \_\|_____/ |_____|
+                         =======================
+                            ReSync Internals
+                            ================
+                             >> Sezei.me <<
+                            
+    ------------------------------------------------------------------------------
+    
+    This Script
+    -----------
+    Authors                 MasterScootScoot                        
+    Description             -
+
+    ------------------------------------------------------------------------------
+
+    >> IMPORTANT <<
+	Your use of this code is subject to ReSync's Terms of Use & Service.
+	See the "About" script in the main model for detailed information regarding
+	the TOS.
+	
+	>> WARNINGS <<
+	- Making changes to this script may make ReSync unstable or completely break it
+	  altogether. 
+	- No support will be made for custom modifications made to this
+	  script.
+	- Do not steal this and attempt to publish it under your name. Doing so will
+	  make you subject to the penalties outlined in the TOS.
+    
+]]
+
+return function()
+	CoreAPI('').Create({
+		Description = '',
+		PermissionLevel = 0,
+		Shorthand = {},
+		Parameters = '',
+		Run = function()
+
+		end,
+	})
+end
