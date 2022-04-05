d449b60af3afb2dadfa0d5773e00306d590b5f7f
MasterKingSirPlease <52339239+MasterKingSirPlease@users.noreply.github.com>
2022-04-05 19:56:58 -0400

    Create _TOS.lua

---
+++ CorePlugins/CoreCommands/About/_TOS.lua
@@ -0,0 +1,102 @@
+return [[<EDIT FONT &SIZE:11>
+These Terms of Service apply to all products and services provided by Project ReSync.
+<RESET FONT>
+<EDIT FONT &SIZE:24>
+Table of Contents
+<INSERT LINE &COLOR:255,255,255>
+<RESET FONT>
+Introduction
+Definitions
+Code Policies
+Accounts
+Punishments
+
+<INSERT TEXT BOX &COLOR:255,128,0>
+<EDIT FONT &SIZE:24>
+Introduction
+<RESET FONT>
+This is also known as "the icky stuff that nobody actually reads but we can refer to if someone breaks the rules."
+<EXIT TEXT BOX>
+<INSERT TEXT BOX &COLOR:60,145,230>
+In other words...
+This is a guide on how to use our product safely without infringing on our rights as developers. You'll also find here a list of your privileges (yes, it IS a privilege to use ReSync and yes, it CAN be taken away) under our jurisdiction. For all intents and purposes, this is not a legal document, but it contains the rules you must follow if you don't want to be banned from using ReSync. We've reviewed it and tried to make it as loophole-free as possible.
+<EXIT TEXT BOX>
+
+<INSERT TEXT BOX &COLOR:255,128,0>
+<EDIT FONT &SIZE:24>
+Definitions
+<RESET FONT>
+The following definitions, regardless of case, may be used to mean the following terms (An indent is used to indicate the continuation of the previous line, and any text in parentheses following the closure of the immediate parentheses is considered commentary and is not a part of the definitions or any content in this commentary and is not a part of the definitions or any content in this document, excluding point a):
+"THE DOCUMENT" or "RULES" or "REGULATIONS" - Refers to any content in this document, excluding point a
+"CODE POLICIES" - Refers to point c of the document
+"DEFINITIONS" Refers to this point of the document (point b, not this sentence, silly)
+"PUNISHMENTS" - Refers to point e of the document
+"THE SYSTEM" or "PROJECT RESYNC" or "RESYNC" or "PRODUCT" or "SERVICE" or "SERVICES" - Refers to the software
+"WE" or "US" or "OUR" or "DEVELOPER" or "WE'RE" - Refers to the owners of the software:
+	MasterScootScoot,0bBinary/EngiAdurite, and the official ReSync support team. We're means "we are" in this case (And really, in all cases).
+"YOU" or "YOUR" or "YOU'RE" - Refers to the person using this software: yourself, the user. You're means "you are" in this case (And really, in all cases...).
+"ONLINE" - Refers to the World Wide Web
+<EXIT TEXT BOX>
+
+<INSERT TEXT BOX &COLOR:255,128,0>
+<EDIT FONT &SIZE:24>
+Code Policies
+<RESET FONT>
+Imagine a world...
+Where everybody is a reasonable actor and nobody has malicious intentions. In this world, creating a service is an engineering experience in which we focus on the technology and enabling others to experience cool stuff instead of a security exercise dictated by the lowest common denominator among the user base. Fun world, no?
+<EXIT TEXT BOX>
+<INSERT TEXT BOX &COLOR:60,145,230>
+Too bad.
+We don't live in that world. Therefore, we present these rules that you must agree to in order to use this service.
+All rules fall into one of two categories:
+1. Don't be a jerk.
+» Do not spend your time discovering all the ways to DOS attack this service. Our endpoint is not the United States Pentagon; our security is not perfect. You ultimately will get nothing of value from doing this.
+» If you happen to learn someone else's credentials, don't upload data to their endpoint without permission from them. That's just annoying, and it forces us to have to waste our time fixing the issue when we could be making the system more awesome.
+» Don't put any illegal content into any of ReSync's components. You're bound by law to not do so.
+» Don't put any content against Roblox's Terms of Service into any of ReSync's components or the Roblox gods will come after you with banhammers galore.
+» Don't flood us with uploads. The service isn't invincible, and Engi spent money on it. Yeah, turns out hosting a website 24/7 isn't free. Fun fact for you there.
+» We can't stop you from doing so, but please don't sell access to plugins. We made ReSync for everybody. Additionally, if you attempt to hide or sell your plugin's code, it will not be eligible for public release.
+2. Don't be an idiot.
+» Don't upload private data. Everything is stored in plain text. This means anything that you upload to the website will be something you're okay with emailing to your worst enemy.
+» Don't rely on the system to be up 24/7, okay? ReSync isn't infallible. Files get deleted, servers get full, things go down. We try not to make the data ephemeral, but we make no guarantees.
+» Don't deploy a service that uses our endpoint or any of ReSync's other internal components without the explicit permission of 0bBinary or MasterScootScoot. This leads to violations of rule #1.
+» Don't push your code to Github or any other service with a link to our endpoint hardcoded in your program. This becomes a problem is your project becomes popular and can lead to violations of all rules. We also have no control over who receives what information in that scenario, and it becomes harder for everyone.
+Violations of these rules lead to people getting banned and features being removed. This sucks for everybody. So don't be a jerk, and don't be an idiot. Thanks to ptsv2.com for helping us with these rules.
+<EXIT TEXT BOX>
+
+<INSERT TEXT BOX &COLOR:255,128,0>
+<EDIT FONT &SIZE:24>
+Accounts
+<RESET FONT>
++ You can have multiple accounts, but it's not recommended. You can separate your settings between certain experiences or groups with a single account.
++ All account usernames must be in accordance with Roblox's Terms of Service. Any inappropriate usernames will be flagged, and you will be restricted from using ReSync until such time as your restriction is appealed.
+ReSync will store your password in a secure database, but our support staff will not have access to view this information (aside from MasterScootScoot, the database developer). ReSync staff will never ask you for your password, as we can perform moderation actions on your account without logging in under your name. This means that anyone asking you for your password is a fraud that is attempting to steal your account. If you happen to forget your password, you may contact us to reset it for you. Our staff will never access your account unless:
+- You give us your expressed consent
+- You or your account has been flagged for suspicious activity
++ If you have multiple Roblox accounts and want to use only one ReSync account, you can do that! ReSync accounts function mostly independently of Roblox accounts. Note that due to this, it is critical that you do not give ANYONE your login information.
++ We at Sezei.me understand how important it is to protect the loss of data. The system is designed in a way so that you may access your saved settings from other Roblox accounts, or experiences not owned by you. If you happen to lose access to your Roblox account, we are not liable for the loss of data. However, you'll still be able to access your account with ReSync. It's recommended that you take a video of you on your account to be able to prove to us that you are the rightful owner of the account in the event that you forget your credentials. We will only access your account, reset your password, and delete your settings for you if you can verify that you are the account owner. Otherwise, there is a security risk in doing so, and we will NOT reset your information. In other words, no support will be provided, so don't come crying to us if you don't have proof of account ownership.
++ Note that there's no "Remember me" option when logging in. This is partially due to the fact that we want to make sure your account is secure with us, and this is also because MasterScootScoot was just too lazy to create it.
++ Due to Roblox limitations, your account unfortunately can't be 100% secure. We'll take responsibility if our system is breached, but it is your responsibility to keep your account with us secure. This means ensuring that you trust your developers, as any game developer will have access to view the scripts, and therefore your product Keys and potentially your account information if you've placed that information there. You should try to keep your Keys as private as possible and only share them on a need-to-know basis. Please remember that if you need to reset your Keys at any time, you may feel free to change them from the Project ReSync Database. If your Key(s) is used by someone else you shared it with without your consent, we will be able to reset it for you, providing that you can confirm your ownership of the account in question.
+<EXIT TEXT BOX>
+
+<INSERT TEXT BOX &COLOR:255,128,0>
+<EDIT FONT &SIZE:24>
+Punishments
+<RESET FONT>
+Ah, the best part! Yes, this is what happens to those who possess no integrity and honor. Penalties for breaking rules will be listed in order of severity, from best case scenario to worst. Think of it like accidental misdemeanor to first degree felony.
+1) Being a Jerk in Your Own Game
+Your game is your game! We'd rather you not banish all of consumer support, loopkill your players, and troll everybody, but that's up to you. This point was just included to make sure you know that you can feel comfortable playing your game as long as you're in compliance with all other rules and regulations. :>
+2) Spamming the Endpoint
+We consider this breaking the number-one rule of "Don't be a jerk." Yes, we realize that some of our code is stored in public resources online. If you're curious about them, feel free to ask a product developer or do some simple experimentation with code, provided you're in compliance with our Code Policies. Don't send more than one request per five seconds to our endpoint or ReSync's Roblox data storage. There's a reason the settings have a timeout interval. We're not going to judiciously monitor all incoming traffic, but if we get a gazillion requests and the system begins to lag, we may look into it. An accidental or single-time offense will result in a reminder. Continued misbehavior will result in a warning that will be logged, and continued refusal to change or remove the item causing excess traffic will result in a ban from our service. The length of the ban is up to the discretion of consumer support.
+3) Product Theft
+We absolutely DO NOT tolerate any form of product theft. Stealing ReSync and publishing it under your own name is completely unacceptable. Make your own [REDACTED DUE TO ROBLOX TOS].
+4) Publishing Internal Code
+We keep our internal code obfuscated to prevent product theft. If you reverse engineer our code and publish it, even if you retain all credit to us, we will permanently ban you from using our services.
+<EXIT TEXT BOX>
+<INSERT TEXT BOX &COLOR:60,145,230>
+-) Support Discretion
+Consumer support has the last say in everything. Don't try to find loopholes in the rules because we'll ban you anyway. If you have a problem with a member of support, you can contact MasterScootScoot.
+<EXIT TEXT BOX>
+
+All of this information can be found again in the Database at any time. If you want to review the Terms of Service, just go to the Help section of the menu. If you still have questions or you're just lazy and didn't want to read this, feel free to contact consumer support. We will try to assist you as best we can... but we'd really rather that you just read the information if possible.
+]]
