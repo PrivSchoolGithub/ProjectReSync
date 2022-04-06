57ba1229f2e9ca99c9e49d8a360fd148b4a52a7d
MasterKingSirPlease <52339239+MasterKingSirPlease@users.noreply.github.com>
2022-04-05 20:01:14 -0400

    Create SystemHint.lua

---
+++ Plugins/Commands/Administration/SystemHint.lua
@@ -0,0 +1,87 @@
+--[[
+                          _____       _____                  
+                         |  __ \     / ____|                 
+                         | |__) |___| (___  _   _ _ __   ___ 
+                         |  _  // _ \\___ \| | | | '_ \ / __|
+                         | | \ \  __/____) | |_| | | | | (__ 
+                         |_|  \_\___|_____/ \__, |_| |_|\___|
+                                             __/ |           
+                                            |___/            
+                        
+            
+            @Description: The revival of SyncAdmin.
+            @Authors:
+            -    Anna [VolcanoINC]
+            -    Marcy [ForPizzaSake]
+            -    Engi [EngiAdurite][Sezei#3061]
+            -    Scoot [MasterScootScoot][MasterKingSirPlease#6533]
+            
+            Intellectual Property of Sezei.me
+    ======================================================================================
+    
+    This Script
+    -----------
+    Authors                 MasterScootScoot
+    Description             Displays hint to everyone in all servers
+    --------------------------------------------------------------------------------------
+    
+    >> Developer Notice <<
+    Please refer to the Documentation script for a full comprehensive guide
+    on how to create custom plugins.
+    IMPORTANT REMINDERS:
+    !! | Don't worry if something appears as an unknown global variable (Like SyncAPI)!
+    It will have no issues running, provided you use the correct syntax.
+    !! | We're here to help!
+    If you need support creating a custom plugin, feel free to DM MasterKingSirPlease#6533 or Sezei#3061.
+    Alternatively, you could join the community server here: https://discord.gg/Y9cu3ZcvvG   
+--]]
+
+return function()
+	local messageService = game:GetService('MessagingService')
+	local subSuccess,subScribeConnection = false
+	local function try()
+		subSuccess,subscribeConnection = pcall(function()
+			return messageService:SubscribeAsync('RS_SysHints',function(receiving)
+				for iteration,target in pairs(game:GetService('Players'):GetPlayers()) do
+					SyncAPI:DisplayNotification(target,{
+						Type = 'Sidebar',
+						Title = 'Broadcast from '..receiving.Data.Admin..'. Click here to view.',
+						Icon = 'Exclamation',
+						Clicked = function()
+							SyncAPI:DisplayNotification(target,{
+								Type = 'Hint',
+								Title = receiving.Data.Admin,
+								Icon = 'Exclamation',
+								Time = 25,
+							})
+						end,
+					})
+				end
+			end)
+		end)
+	end
+	repeat
+		try()
+	until subSuccess
+	
+	SyncAPI('SystemHint').Create({
+		Description = 'Broadcasts a system-wide hint to everyone in ALL servers for twenty-five seconds.',
+		PermissionLevel = 2,
+		Shorthand = {'SH','BroadcastHint','BcH'},
+		Parameters = 'message',
+		Run = function(main,user,...)
+			local sending = table.concat({...},' ') -- Cannot use ... outside vararg func
+			local success,issue = pcall(function()
+				messageService:PublishAsync('RS_SysHints',{
+					Admin = user.DisplayName,
+					Message = sending,
+				})
+			end)
+			if success then
+				return true,'Broadcasting your hint to all servers now. (Give it a moment)',10
+			else
+				return false,issue,10
+			end
+		end,
+	})
+end
