22283ecf1c09eaf034b52a9ee53d22455d6357e2
MasterKingSirPlease <52339239+MasterKingSirPlease@users.noreply.github.com>
2022-04-05 20:06:24 -0400

    Create Jump.lua

---
+++ Plugins/Commands/Utility/Jump.lua
@@ -0,0 +1,62 @@
+--[[
+                          _____       _____                  
+                         |  __ \     / ____|                 
+                         | |__) |___| (___  _   _ _ __   ___ 
+                         |  _  // _ \\___ \| | | | '_ \ / __|
+                         | | \ \  __/____) | |_| | | | | (__ 
+                         |_|  \_\___|_____/ \__, |_| |_|\___|
+                                             __/ |           
+                                            |___/            
+                        
+            
+            @Description: The revival of SyncAdmin.
+            @Authors:
+            -    Anna [VolcanoINC]
+            -    Marcy [ForPizzaSake]
+            -    Engi [EngiAdurite][Sezei#3061]
+            -    Scoot [MasterScootScoot][MasterKingSirPlease#6533]
+            
+            Intellectual Property of Sezei.me
+    ======================================================================================
+    
+    This Script
+    -----------
+    Authors                 MasterScootScoot
+    Description             f0rc3 jmp
+    --------------------------------------------------------------------------------------
+    
+    >> Developer Notice <<
+    Please refer to the Documentation script for a full comprehensive guide
+    on how to create custom plugins.
+    IMPORTANT REMINDERS:
+    !! | Don't worry if something appears as an unknown global variable (Like SyncAPI)!
+    It will have no issues running, provided you use the correct syntax.
+    !! | We're here to help!
+    If you need support creating a custom plugin, feel free to DM MasterKingSirPlease#6533 or Sezei#3061.
+    Alternatively, you could join the community server here: https://discord.gg/Y9cu3ZcvvG   
+--]]
+
+return function()
+	SyncAPI('Jump').Create({
+		Description = 'Makes the specified user(s) jump. If no user is specified, you will jump.',
+		PermissionLevel = 1,
+		Shorthand = 'Up',
+		Parameters = 'optional:user(s)',
+		Run = function(main,user,users)
+			if not users then
+				users = {user}
+			end
+			
+			local list = {}
+			for iteration,target in pairs(users) do
+				table.insert(list,target.DisplayName)
+				local char = target.Character
+				
+				if char ~= nil then
+					char:FindFirstChild('Humanoid').Jump = true
+				end
+			end
+			return true,'Made '..table.concat(list,', ')..' jump.',8,users,user.DisplayName..' has made you jump.',8
+		end,
+	})
+end
