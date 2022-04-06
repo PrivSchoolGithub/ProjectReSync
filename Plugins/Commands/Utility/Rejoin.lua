91184a6d611c729b06a6221f139ecb75d15e229e
MasterKingSirPlease <52339239+MasterKingSirPlease@users.noreply.github.com>
2022-04-05 20:08:10 -0400

    Create Rejoin.lua

---
+++ Plugins/Commands/Utility/Rejoin.lua
@@ -0,0 +1,52 @@
+--[[
+                          _____       _____                  
+                         |  __ \     / ____|                 
+                         | |__) |___| (___  _   _ _ __   ___ 
+                         |  _  // _ \\___ \| | | | '_ \ / __|
+                         | | \ \  __/____) | |_| | | | | (__ 
+                         |_|  \_\___|_____/ \__, |_| |_|\___|
+                                             __/ |           
+                                            |___/            
+                        
+            
+            @Description: The revival of SyncAdmin.
+            @Authors:
+            -    Anna [VolcanoINC]
+            -    Marcy [ForPizzaSake]
+            -    Engi [EngiAdurite][Sezei#3061]
+            -    Scoot [MasterScootScoot][MasterKingSirPlease#6533]
+            
+            Intellectual Property of Sezei.me
+    ======================================================================================
+    
+    This Script
+    -----------
+    Authors                 MasterScootScoot
+    Description             Rejoin command for ReSync
+    --------------------------------------------------------------------------------------
+    
+    >> Developer Notice <<
+    Please refer to the Documentation script for a full comprehensive guide
+    on how to create custom plugins.
+    IMPORTANT REMINDERS:
+    !! | Don't worry if something appears as an unknown global variable (Like SyncAPI)!
+    It will have no issues running, provided you use the correct syntax.
+    !! | We're here to help!
+    If you need support creating a custom plugin, feel free to DM MasterKingSirPlease#6533 or Sezei#3061.
+    Alternatively, you could join the community server here: https://discord.gg/Y9cu3ZcvvG   
+--]]
+
+return function()
+	SyncAPI('ReJoin').Create({
+		Description = 'Makes you rejoin the current server.',
+		PermissionLevel = 0,
+		Shorthand = 'Rj',
+		Run = function(main,user)
+			if game:GetService('RunService'):IsStudio() then
+				return false,'Cannot use TeleportService in Studio.',5
+			end
+			game:GetService('TeleportService'):Teleport(game.PlaceId,user)
+			return true,'Force rejoining the user...'
+		end,
+	})
+end
